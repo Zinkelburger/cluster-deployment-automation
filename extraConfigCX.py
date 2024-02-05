cad7cce19d37921d4ff1ce3da966c9f210fa43c0
Zinkelburger <anbernal@redhat.com>
2024-02-05 14:09:33 -0500

    adding support for updating cx5 & 6
    - need to put cx_fwup and cx_fwdefaults in dpu-tools

--- extraConfigCX.py
+++ extraConfigCX.py
@@ -14,13 +14,19 @@ from clustersConfig import ExtraConfigArgs
 The "ExtraConfigCX" is used to put the CX in a known good state. This is achieved by
 1) Having a CoreOS Fedora image ready and mounted on NFS. This is needed for loading
 a known good state on each of the workers.
-2) The scripts will try to update the firmware of the CX with mlxup.
-3) Then the worker node is cold booted. This will also cold boot the CX.
+2) Then SSH-ing into the load CoreOS Fedora image, we can run a pod with all the BF2
+tools available. https://github.com/bn222/dpu-tools/
+3) The scripts will try to update the firmware of the CX with mlxup.
+4) Then the worker node is cold booted. This will also cold boot the CX.
 """
 
 
 def ExtraConfigCX(cc: ClustersConfig, _: ExtraConfigArgs, futures: Dict[str, Future[None]]) -> None:
+    coreosBuilder.ensure_fcos_exists()
     logger.info("Updating CX firmware on all workers")
+    lh = host.LocalHost()
+    nfs = NFS(lh, cc.external_port)
+    iso_url = nfs.host_file("/root/iso/fedora-coreos.iso")
 
     def helper(h: host.HostWithCX) -> None:
         def check(result: host.Result) -> None:
@@ -28,6 +34,7 @@ def ExtraConfigCX(cc: ClustersConfig, _: ExtraConfigArgs, futures: Dict[str, Fut
                 logger.info(result)
                 sys.exit(-1)
 
+        h.boot_iso_redfish(iso_url)
         h.ssh_connect("core")
         check(h.cx_firmware_upgrade())
         h.cold_boot()
